0119c6fa492499db734153711af3daedc32eaa33
stonedcoder <vraunaq1213@ghmail.com>
2018-01-29 14:58:08 +0530

    Numpy & matplot lib ref. added .

---
+++ Lecture_02/Numpy&matplotlib.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+my_list = [1,2,3,4]
+print type(my_list)
+my_listarr = np.array(my_list)
+print my_listarr
+print type(my_listarr)
+print my_listarr.shape
