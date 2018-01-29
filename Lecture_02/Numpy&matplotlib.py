0966f490d1e0a0815561064a91ca8dac1002a537
stonedcoder <vraunaq1213@ghmail.com>
2018-01-29 15:00:12 +0530

    Array list print

--- Lecture_02/Numpy&matplotlib.py
+++ Lecture_02/Numpy&matplotlib.py
@@ -8,3 +8,14 @@ my_listarr = np.array(my_list)
 print my_listarr
 print type(my_listarr)
 print my_listarr.shape
+
+arr = np.zeros((4,2), dtype=np.uint8)
+print arr.shape
+print arr.dtype
+
+my_arr = np.zeros((4,2))
+print my_arr.shape
+my_arr[:,0] +=1.0
+my_arr[:,1] +=2.0
+print np.unique(my_arr, return_counts=True)
+# print my_arr
