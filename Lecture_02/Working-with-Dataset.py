529d29f4db41e0e8cdc333e7056ba6cebf3583d3
stonedcoder <vraunaq1213@ghmail.com>
2018-01-29 15:30:50 +0530

    Getting the maximum length , minimum lengh and mean length .
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_02/Working-with-Dataset.py
+++ Lecture_02/Working-with-Dataset.py
@@ -22,3 +22,32 @@ titles = ds.get('movie_title')
 print len(titles)
 print type(titles)
 
+count = 0
+for ix in titles:
+    if count<=10:
+        print ix
+    count+=1
+
+
+#The above written code should return the following . 
+#Avatar 
+#Pirates of the Caribbean: At World's End 
+#Spectre 
+#The Dark Knight Rises 
+#Star Wars: Episode VII - The Force Awakens             
+#John Carter 
+#Spider-Man 3 
+#Tangled 
+#Avengers: Age of Ultron 
+#Harry Potter and the Half-Blood Prince 
+#Batman v Superman: Dawn of Justice 
+
+
+len_titles = []
+for ix in titles:
+    len_titles.append(len(ix))
+#print len(len_titles)
+len_titles = np.array(len_titles)
+print len_titles.max()
+print len_titles.min()
+print len_titles.mean()
