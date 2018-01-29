74ef75957347210497228d948f0d83b6c745e48d
stonedcoder <vraunaq1213@ghmail.com>
2018-01-29 15:24:58 +0530

    Reading the dataset
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_02/Working-with-Dataset.py
+++ Lecture_02/Working-with-Dataset.py
@@ -3,3 +3,12 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 import pandas as pd
 
+ds = pd.read_csv('./movie_metadata.csv')
+print type(ds)
+print len(ds.columns)
+
+data = ds.values
+print type(data)
+print data.shape
+#ds.head(n=5)
+ds.tail(n=3)
