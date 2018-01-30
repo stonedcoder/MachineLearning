2201f89e765eba50a2f7efca07119b27ca55d45d
stonedcoder <vraunaq1213@ghmail.com>
2018-01-30 11:33:49 +0530

    Plotting images
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_02/WorkingwithOpenCV.py
+++ Lecture_02/WorkingwithOpenCV.py
@@ -20,3 +20,19 @@ def rectify(h):
     hnew[3] = h[np.argmax(diff)]
 
     return hnew
+
+
+
+# Add image here. Can also use laptop's webcam if the resolution is good enough to capture readable document content
+image = cv2.imread('./sample-1.jpg')
+print image.shape
+
+# Resize image so it can be processed. Choose optimal dimensions such that important content is not lost
+image = cv2.resize(image, (1500, 880))
+orig = image.copy()
+
+plt.figure(0)
+plt.imshow(image)
+plt.show()
+
+print image.shape
