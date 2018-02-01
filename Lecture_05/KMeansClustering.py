eee472e2508299e612519de43dd4bea3ee27f5c8
stonedcoder <vraunaq1213@ghmail.com>
2018-02-01 21:20:32 +0530

    Scattering Plots added
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_05/KMeansClustering.py
+++ Lecture_05/KMeansClustering.py
@@ -13,3 +13,8 @@ dist_01 = np.random.multivariate_normal(mean_01, cov_01, 500)
 dist_02 = np.random.multivariate_normal(mean_02, cov_02, 500)
 
 print dist_01.shape, dist_02.shape
+
+plt.scatter(dist_01[:,0],dist_01[:,1], color='red')
+plt.scatter(dist_02[:,0],dist_02[:,1], color='blue')
+
+plt.show()
