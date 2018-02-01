41aa45f894102d38347bc7a2205ae149c4dcf98e
stonedcoder <vraunaq1213@ghmail.com>
2018-02-01 21:17:39 +0530

    One dimensional normal distribution
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_05/KMeansClustering.py
+++ Lecture_05/KMeansClustering.py
@@ -2,3 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
 
+# Define a dataset
+mean_01 = np.array([1.0, 0.5])
+cov_01 = np.array([[1.0,0.1],[0.1,1.2]])
+
+mean_02 = np.array([5.3,6.5])
+cov_02 = np.array([[1.0,0.1],[0.1,1.2]])
+
+dist_01 = np.random.multivariate_normal(mean_01, cov_01, 500)
+dist_02 = np.random.multivariate_normal(mean_02, cov_02, 500)
+
+print dist_01.shape, dist_02.shape
