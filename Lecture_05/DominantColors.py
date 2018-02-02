c24c686d491aadd22dec583a850cf7692e6c5750
stonedcoder <vraunaq1213@ghmail.com>
2018-02-02 15:32:43 +0530

    Reading the dataset image
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_05/DominantColors.py
+++ Lecture_05/DominantColors.py
@@ -3,3 +3,18 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 import cv2
 from sklearn.cluster import KMeans
+
+## reading the image .
+im = cv2.imread('./im.jpg')
+im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+
+out_r = 100
+im = cv2.resize(im, (int(out_r*float(c)/r), out_r))
+print im.shape
+
+pixels = im.reshape((-1, 3))
+print pixels.shape
+
+plt.imshow(im)
+plt.show()
+
