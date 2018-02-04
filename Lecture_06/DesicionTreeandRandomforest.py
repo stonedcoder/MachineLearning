af9ed4717a5a085f0ef1d269073b53cb43fb3342
stonedcoder <vraunaq1213@ghmail.com>
2018-02-04 23:54:42 +0530

    Getting started with decision tree classifier
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_06/DesicionTreeandRandomforest.py
+++ Lecture_06/DesicionTreeandRandomforest.py
@@ -37,3 +37,34 @@ df = df.dropna(axis=0)
 
 data = df.values
 print data.shape
+
+
+## printing test and train set images
+X = data[:,1:] ## input
+Y = data[:, 0] ## labels
+
+print X.shape
+print Y.shape
+
+split = int(0.8*X.shape[0])
+
+X_train = X[:split]
+Y_train = Y[:split]
+
+X_test = X[split:]
+Y_test = Y[split:]
+
+print "-----------"
+
+print X_train.shape
+print X_test.shape
+
+print Y_train.shape
+print Y_test.shape
+
+
+### Getting started with decision tree classifier 
+import sklearn
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
