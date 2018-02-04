5aa9a5e165371655f61b81c0d8f9d5fb5a7d6d84
stonedcoder <vraunaq1213@ghmail.com>
2018-02-04 23:18:48 +0530

    Reading the dataset .
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_06/DesicionTreeandRandomforest.py
+++ Lecture_06/DesicionTreeandRandomforest.py
@@ -2,3 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 %matplotlib inline
+
+
+ds = pd.read_csv('./titanic.csv')
+print ds.columns
+print len(ds.columns)
+ds.head()
