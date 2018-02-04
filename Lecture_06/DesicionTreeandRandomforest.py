c861e4ad90ec7dfe3d4b56bd5445a0becc3954a8
stonedcoder <vraunaq1213@ghmail.com>
2018-02-04 23:38:19 +0530

    Sex mapping of passengers
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_06/DesicionTreeandRandomforest.py
+++ Lecture_06/DesicionTreeandRandomforest.py
@@ -3,8 +3,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 %matplotlib inline
 
-
+##reading titanic .
 ds = pd.read_csv('./titanic.csv')
 print ds.columns
 print len(ds.columns)
 ds.head()
+
+
+cols_to_remove = [
+    'PassengerId',
+    'Name',
+    'Cabin',
+    'Embarked',
+    'Ticket'
+]
+df = ds.drop(cols_to_remove, axis=1)
+print len(df.columns)
+
+df.head()
+
+# sex mapping .
+sex_mapping = {
+    'male': 0,
+    'female': 1,
+}
+df.Sex = df.Sex.map(sex_mapping)
