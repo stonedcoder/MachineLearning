5e0eb2f70f9ef80fed53704c2d982af23253c68a
stonedcoder <vraunaq1213@ghmail.com>
2018-02-06 22:27:09 +0530

    Splitting the dataset
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_07/LinearRegression.py
+++ Lecture_07/LinearRegression.py
@@ -2,3 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
 from sklearn.datasets import make_regression
+
+X, y = make_regression(n_samples=500, n_features=1, bias=4.2,noise=5.1)
+print X.shape, y.shape
+
+split = int(0.8*X.shape[0])
+print split
+
+X_train = X[:split]
+X_test = X[split:]
+
+y_train = y[:split]
+y_test = y[split:]
+
+print X_train.shape
+print X_test.shape
+
+print y_test.shape
