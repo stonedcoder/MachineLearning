c504cff3de344a998592b8371628376cba1e84cd
stonedcoder <vraunaq1213@ghmail.com>
2018-02-11 10:46:32 +0530

    Scattered plots , printing circles
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_09/NNwithKeras.py
+++ Lecture_09/NNwithKeras.py
@@ -6,3 +6,10 @@ import keras
 from keras.utils import np_utils
 from keras.layers import Dense, Activation
 from keras.models import Sequential
+
+X, y = make_circles(n_samples=1000, factor=0.4)
+print X.shape, y.shape 
+
+plt.scatter(X[:, 0], X[:, 1])
+plt.show()
+
