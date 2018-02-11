ac17606207402f3528365840260d9f7c0302b4c3
stonedcoder <vraunaq1213@ghmail.com>
2018-02-11 10:41:28 +0530

    NeuralNetworks , with Keras introduction
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

---
+++ Lecture_09/NNwithKeras.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+from sklearn.datasets import make_circles
+import keras
+from keras.utils import np_utils
+from keras.layers import Dense, Activation
+from keras.models import Sequential
