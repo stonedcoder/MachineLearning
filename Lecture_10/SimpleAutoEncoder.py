69904e5ed76a8d4c13925c4e165b79cb7d80fb4e
stonedcoder <vraunaq1213@ghmail.com>
2018-03-11 11:25:12 +0530

    Activation of sigmoid
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_10/SimpleAutoEncoder.py
+++ Lecture_10/SimpleAutoEncoder.py
@@ -7,3 +7,14 @@ import pandas as pd
 ds = pd.read_csv('../../mnist_train.csv')
 data = ds.values[100:4600,1:]/255.0
 print data.shape
+
+
+inp = Input(shape=(784,))
+h1 = Dense(100)
+a1 = Activation('sigmoid')
+y = Dense(784)
+ya = Activation('sigmoid')
+
+yout =  ya(y(a1(h1(inp))))
+model = Model(input=[inp], output=[yout])
+model.summary()
