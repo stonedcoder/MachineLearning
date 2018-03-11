72a400adcdcd87fd6648374d8d9443288799f72c
stonedcoder <vraunaq1213@ghmail.com>
2018-03-11 11:33:30 +0530

    Adding encoder and decoder
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_10/SimpleAutoEncoder.py
+++ Lecture_10/SimpleAutoEncoder.py
@@ -25,3 +25,14 @@ model.compile(optimizer='adam', loss='MSE', metrics=['accuracy'])
 split = int(0.8*data.shape[0])
 
 model.fit(data[:split], data[:split],nb_epoch=60, batch_size=30,verbose=2, validation_data=(data[split:], data[split:]))
+
+
+
+##Encoder 
+enc_out = a1(h1(inp))
+encoder = Model(input=[inp], output=[enc_out])
+
+##Decoder
+dec_inp = Input(shape=(100,))
+dec_out = ya(y(a1(dec_inp)))
+decoder = Model(input=[dec_inp], output=[dec_out])
