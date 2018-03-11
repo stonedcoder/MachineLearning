87a3909d0068dc5bf34ffe81c4857b70b255c71c
stonedcoder <vraunaq1213@ghmail.com>
2018-03-11 13:16:47 +0530

    K image Dataset added
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_10/ConvolutionNN.py
+++ Lecture_10/ConvolutionNN.py
@@ -13,3 +13,9 @@ K = np.array([[-0,-1, 0],[-1, 4, -1], [0,-1,0]])
 print K
 print K.shape
 
+out_r = img.shape[0] - K.shape[0] + 1
+out_c = img.shape[1] - K.shape[1] + 1
+X = np.zeros((out_r, out_c))
+print img.shape
+print X.shape
+
