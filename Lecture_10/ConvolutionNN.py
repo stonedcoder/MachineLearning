e03daefe78fd6cd8a381a72df1565e14ad04bdbd
stonedcoder <vraunaq1213@ghmail.com>
2018-03-11 11:50:06 +0530

    reading image
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_10/ConvolutionNN.py
+++ Lecture_10/ConvolutionNN.py
@@ -2,3 +2,9 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 %matplotlib inline
+
+
+im = cv2.imread('/home/Stonedcoder/Perceptron_Summe/class_11/img.jpg')
+img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+plt.imshow(img, cmap='gray')
+plt.show()
