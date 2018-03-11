5e99a4ecdc6f4839d8dcd9beba05cf13b497871c
stonedcoder <vraunaq1213@ghmail.com>
2018-03-11 12:50:48 +0530

    K np array
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_10/ConvolutionNN.py
+++ Lecture_10/ConvolutionNN.py
@@ -8,3 +8,8 @@ im = cv2.imread('/home/Stonedcoder/Perceptron_Summe/class_11/img.jpg')
 img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 plt.imshow(img, cmap='gray')
 plt.show()
+
+K = np.array([[-0,-1, 0],[-1, 4, -1], [0,-1,0]])
+print K
+print K.shape
+
