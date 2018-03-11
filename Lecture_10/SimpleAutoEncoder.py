cd3ccdef51c9f824da00502321b841b30540f7dd
stonedcoder <vraunaq1213@ghmail.com>
2018-03-11 11:22:56 +0530

    MNIST_NN dataset added and reading the dataset in ecoder
    Signed-off-by: stonedcoder <vraunaq1213@ghmail.com>

--- Lecture_10/SimpleAutoEncoder.py
+++ Lecture_10/SimpleAutoEncoder.py
@@ -3,3 +3,7 @@ from keras.layers import Input, Dense, Activation
 from keras.models import Model
 from keras.utils import np_utils
 import pandas as pd
+
+ds = pd.read_csv('../../mnist_train.csv')
+data = ds.values[100:4600,1:]/255.0
+print data.shape
